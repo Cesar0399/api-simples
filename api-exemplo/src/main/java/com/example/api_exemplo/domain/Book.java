9f007b71304f02503881378aa910f55b2d5f6e31
Waldo Cesar <waldo_cesar@outlook.com>
2026-09-01 18:03:36 -0300

    api criada

---
+++ api-exemplo/src/main/java/com/example/api_exemplo/domain/Book.java
@@ -0,0 +1,20 @@
+package com.example.api_exemplo.domain;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+       public String title;
+       public String author;
+       public String area;
+       public String publisher;
+       @JsonProperty("year_of_publisher")
+       public String yearOfPublish;
+       public String isbn;
+
+
+}
